755887e33d1db268ef295c1a7d2047d2a3e28710
Juliano <c.juliano1991@gmail.com>
2021-07-23 20:01:41 -0300

    finish???

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,1 +1,14 @@
 /* Coloque o JS neste arquivo */
+
+const btn = document.querySelector(".mobile-menu-icon");
+const menuMobile = document.querySelector(".menu-container");
+
+function tooGleMenu() {
+	if (menuMobile.style.display === "flex") {
+		menuMobile.style.display = "none";
+	} else {
+		menuMobile.style.display = "flex";
+	}
+}
+
+btn.addEventListener("click", () => tooGleMenu());
